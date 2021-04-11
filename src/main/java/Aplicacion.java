8dc8386bf0eea452b0ba3aeb6a8c4a7f883ba8f3
franMV4 <79657510+franMV4@users.noreply.github.com>
2021-04-11 19:34:20 +0200

    Registros de las clases

--- src/main/java/Aplicacion.java
+++ src/main/java/Aplicacion.java
@@ -6,6 +6,22 @@ import com.SanValero.Ordenador;
 
 public class Aplicacion {
     public static void main(String[]args){
+        Ordenador ordenador;
+        Cliente cliente;
+        Empleado empleado;
+
+    ordenador = new Ordenador("Intel", "z40", 754, "Ram", true, "Intel");
+    cliente = new Cliente("Jorge", "Argualas", 34, true, "Maria");
+    empleado = new Empleado("Adrian", "74642478C", 67, false, "75348545H");
+
+    ordenador.getCausa();
+
+
+    ordenador.piezasMarcaIguales();
+    cliente.nombresIguales();
+    empleado.DniAsociadosTarjeta();
+
+
 
 
 
